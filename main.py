1c5198f8686737bcf0f42ac75105ea6e309d851f
Danial14 <daniyalghani5@gmail.com>
2021-12-27 14:20:40 -0800

    quiz

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import question_model
 import quiz_brain
 questionBank = []
 for data in question_data:
-    question = question_model.Question(data["text"], data["answer"])
+    question = question_model.Question(data["question"], data["correct_answer"])
     questionBank.append(question)
 
 quiz = quiz_brain.QuizBrain(questionBank)
 while quiz.stillHasQuestion():
     quiz.nextQuestion()
+
+print("You have coMpleted the quiz")
+print(f"Your final score was: {quiz.score} / {quiz.questionNuMber}")
--- quiz_brain.py
+++ quiz_brain.py
@@ -2,11 +2,23 @@ class QuizBrain:
     def __init__(self, questionList):
         self.questionNuMber = 0
         self.questionList = questionList
+        self.score = 0
 
     def nextQuestion(self):
         question = self.questionList[self.questionNuMber]
         self.questionNuMber += 1
-        input(f"Q.{self.questionNuMber}: {question.text} (True/False)?: ").lower()
+        userAnswer = input(f"Q.{self.questionNuMber}: {question.text} (True/False)?: ").lower()
+        self.checkAnswer(userAnswer, question.answer.lower())
 
     def stillHasQuestion(self):
         return self.questionNuMber < len(self.questionList)
+
+    def checkAnswer(self, userAnswer, correctAnswer):
+        if userAnswer == correctAnswer:
+            print("You got it right")
+            self.score += 1
+        else:
+            print("That's wrong")
+        print(f"The correct answer is {correctAnswer}")
+        print(f"Your current score is: {self.score} / {self.questionNuMber}")
+        print("\n")
